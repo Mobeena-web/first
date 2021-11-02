1a2a6121c5fe31b3c762927f16c666439f309a95
Mobeena <mobeenaafzal234@gmail.com>
2021-11-02 01:28:08 -0700

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,40 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'cycle',
+    loadChildren: () => import('./pages/cycle/cycle.module').then( m => m.CyclePageModule)
+  },
+  {
+    path: 'ovulation',
+    loadChildren: () => import('./pages/ovulation/ovulation.module').then( m => m.OvulationPageModule)
+  },
+  {
+    path: 'pregnancy',
+    loadChildren: () => import('./pages/pregnancy/pregnancy.module').then( m => m.PregnancyPageModule)
+  },
+  {
+    path: 'infertility',
+    loadChildren: () => import('./pages/infertility/infertility.module').then( m => m.InfertilityPageModule)
+  },
+  {
+    path: 'safe-period',
+    loadChildren: () => import('./pages/safe-period/safe-period.module').then( m => m.SafePeriodPageModule)
   }
 ];
 
